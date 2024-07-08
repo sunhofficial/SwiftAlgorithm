05d19a63a8b07b17abc9fd1d2e5f00d00b7752f8
Sunho <sunh803@naver.com>
2024-07-08 21:31:34 +0900

    2트: Fail soultion .....

--- Alagorithm_swift/BFS,DFS/프로그래머스/main.swift
+++ Alagorithm_swift/BFS,DFS/프로그래머스/main.swift
@@ -1,53 +1,29 @@
-//
-//  main.swift
-//  Alagorithm_swift
-//
-//  Created by 235 on 7/8/24.
-//
-
-
 import Foundation
 
 func solution(_ tickets:[[String]]) -> [String] {
-    var dicts = [String: [String]]()
-    for ticket in tickets {
-        dicts[ticket[0], default: []].append(ticket[1])
-    }
-    print(dicts)
-       for key in dicts.keys {
-        dicts[key]!.sort()
-    }
-     print(dicts)
-    var queue = ["ICN"]
-    var index = 0
-
-//    func dfs(_ current: String) -> Bool {
-//        if queue.count == tickets.count + 1 {
-//            return true
-//        }
-//        guard let desti = dicts[current] else {return false}
-//        for i in 0..<desti.count {
-//            let next = desti[i]
-//            dicts[current]?.remove(at: i)
-//            queue.append(next)
-//            if dfs(next) {
-//                return true
-//            }
-//
-//        }
-//    }
-    while index < queue.count  {
-        if index == tickets.count + 1 {
-            return queue
+    var answer = [String]()
+    var visited = Array(repeating: false, count: tickets.count)
+    let tickets = tickets.sorted {$0[1] < $1[1]}
+    func dfs(name: String) {
+        if tickets.count == answer.count {
+            answer.append(name)
+            return
         }
-        let current = queue[index]
-        index += 1
-        if let destinations = dicts[current], destinations.count > 0 {
-            queue.append(destinations[0])
-            dicts[current]!.removeFirst()
+        for i in 0..<tickets.count  {
+            let start = tickets[i][0]
+            let end = tickets[i][1]
+            if name == start, !visited[i] {
+                visited[i] = true
+                answer.append(start)
+                dfs(name: end)
+                if tickets.count + 1 == answer.count {
+                    return
+                }
+                visited[i] = false
+                answer.removeLast()
+            }
         }
     }
-    return queue
+    dfs(name: "ICN")
+    return answer
 }
-
-print(solution([["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], ["BOO", "DOO"], ["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]]))
