c33c7e23ac6c7dbad0e40e481e89496c548d5e0c
Sunho <sunh803@naver.com>
2024-07-11 21:05:24 +0900

    2트: 9184 DP

--- Alagorithm_swift/DP/백준/main.swift
+++ Alagorithm_swift/DP/백준/main.swift
@@ -1,38 +1,34 @@
 //
-//  11051.swift
+//  9184.swift
 //  Alagorithm_swift
 //
-//  Created by 235 on 7/10/24.
+//  Created by 235 on 7/11/24.
 //
 
 import Foundation
-let n = Int(readLine()!)!
-let nums = readLine()!.split(separator: " ").map {Int(String($0))!}
-var dp = [Int]()
-func upperBound(find: Int, numbers: [Int]) -> Int {
-    var start = 0
-    var end = numbers.count - 1
-    var mid = 0
-    while start < end {
-        mid = (start + end) / 2
-        if find < numbers[mid] {
-            start = mid + 1
-        } else {
-            end = mid
-        }
+var arr = Array(repeating: Array(repeating: Array(repeating: -1, count: 21), count: 21), count: 21)
+func w(_ a: Int, _ b: Int, _ c: Int) -> Int {
+    if a<=0 || b<=0 || c<=0 {
+        return 1
+    } else if a > 20 || b > 20 || c > 20 {
+        return w(20,20,20)
+    } 
+    if arr[a][b][c]  != -1 {
+        return arr[a][b][c]
     }
-    return end
-}
-for num in nums {
-    if dp.isEmpty {
-        dp.append(num)
-    }
-    if num < dp.last! {
-        dp.append(num)
+        else if a<b && b<c {
+        arr[a][b][c] = w(a,b, c-1) + w(a,b-1, c-1) - w(a,b-1,c)
     } else {
-        let index = upperBound(find: num, numbers: dp)
-        dp[index] = num
+        arr[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    }
+    return arr[a][b][c]
+}
+while true {
+    let abc = readLine()!.split(separator: " ").map { Int(String($0))! }
+    if abc[0] == -1 && abc[1] == -1 && abc[2] == -1 {
+        break
+    }else {
+        let (a,b,c) = (abc[0], abc[1],abc[2])
+        print("w(\(a), \(b), \(c)) = \(w(a, b, c))")
     }
 }
-
-print(dp.count)
