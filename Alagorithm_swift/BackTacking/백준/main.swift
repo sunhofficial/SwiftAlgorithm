646b5fc17f476d0b2d53b631a0eae6e28379217c
Sunho <sunh803@naver.com>
2024-07-20 19:06:27 +0900

    2트: 10974 using visited BackTracking DFS

--- Alagorithm_swift/BackTacking/백준/main.swift
+++ Alagorithm_swift/BackTacking/백준/main.swift
@@ -6,17 +6,35 @@
 //
 
 import Foundation
+//let n = Int(readLine()!)!
+//var arr = Array(1...n)
+//func dfs(_ k: Int) {
+//    if k == n {
+//        print(arr.map { String($0)}.joined(separator: " "))
+//        return
+//    }
+//    for i in k..<n {
+//        if arr[i] > arr[k] {
+//            arr.swapAt(k, i)
+//            dfs(k+1)
+//            arr.swapAt(k, i)
+//        }
+//    }
+//}
+//dfs(0)
 let n = Int(readLine()!)!
-var arr = Array(1...n)
-func dfs(_ k: Int) {
-    if k == n {
-        print(arr.map { String($0)}.joined(separator: " "))
+var answer = ""
+var visited = Array(repeating: false, count: n)
+func dfs(_ ans: String , _ cnt: Int) {
+    if cnt == n {
+        answer += ans + "\n"
         return
     }
-    for i in k..<n {
-        arr.swapAt(k, i)
-        dfs(k+1)
-        arr.swapAt(k, i)
+    for i in 0..<n where !visited[i] {
+        visited[i] = true
+        dfs(ans + "\(i + 1)", cnt + 1)
+        visited[i] = false
     }
 }
-dfs(0)
+dfs("", 0)
+print(answer)
