8c3d753ca3c287b9466d61718a5eaab898499d25
Sunho <sunh803@naver.com>
2024-07-30 14:02:27 +0900

    1트: 1965 dp

--- Alagorithm_swift/DP/백준/main.swift
+++ Alagorithm_swift/DP/백준/main.swift
@@ -1,39 +1,21 @@
 //
-//  9252.swift
+//  1965.swift
 //  Alagorithm_swift
 //
-//  Created by 235 on 7/29/24.
+//  Created by 235 on 7/30/24.
 //
 
 import Foundation
-let firstline = [""] + readLine()!.map{String($0)}
-let secondLine = [""] +  readLine()!.map{String($0)}
-let  n = firstline.count
-let m = secondLine.count
-var dp = Array(repeating: Array(repeating: 0, count: n), count: m)
-for y in 1..<m {
-    for x in 1..<n {
-        if secondLine[y] == firstline[x] {
-            dp[y][x] = dp[y-1][x-1] + 1
-        } else {
-            dp[y][x] = max(dp[y-1][x], dp[y][x-1])
-        }
+let n = Int(readLine()!)!
+let boxs = readLine()!.split(separator: " ").map{ Int(String($0))!}
+var dp = Array(repeating: 1, count: 1001)
+var maxVal = 1
+
+for i in 0..<n{
+
+    for j in 0..<i {
+        if boxs[j] < boxs[i] && dp[i] <= dp[j] { dp[i] = dp[j] + 1 }
     }
+    if maxVal < dp[i] { maxVal = dp[i]}
 }
-var ans = dp.flatMap {$0}.max()!
-print(ans)
-var lcs = ""
-var y = m - 1
-var x = n - 1
-while y > 0 && x > 0 {
-    if secondLine[y] == firstline[x] {
-         lcs = String(secondLine[y]) + lcs
-         y -= 1
-         x -= 1
-     } else if dp[y-1][x] > dp[y][x-1] {
-         y -= 1
-     } else {
-         x -= 1
-     }
-}
-print(lcs)
+print(maxVal)
