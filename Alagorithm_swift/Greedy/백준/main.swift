50c47591be0ba1a439b25990d4b83c2d2c14c1cd
Sunho <sunh803@naver.com>
2024-08-01 16:50:33 +0900

    2트: Fail 11000 time over using tuple

--- Alagorithm_swift/Greedy/백준/main.swift
+++ Alagorithm_swift/Greedy/백준/main.swift
@@ -7,29 +7,39 @@
 
 import Foundation
 let n = Int(readLine()!)!
-var greedyArr = [[Int]]()
+var intervals = [(Int,Int)]()
 for _ in 0..<n {
     let st = readLine()!.split(separator: " ").map{ Int(String($0))!}
-    greedyAlgoritim(nums: st)
+    intervals.append((st[0], st[1]))
 }
-
-func greedyAlgoritim(nums: [Int]) {
-    if greedyArr.isEmpty {
-        greedyArr.append(nums)
+intervals.sort {$0.0 < $1.0}
+var endtimes = [Int]()
+for interval in intervals {
+    if let earliesEnd = endtimes.first, earliesEnd <= interval.0 {
+        endtimes[0] = interval.1
     } else {
-        var lastval = 0
-        var adding = false
-        for i in 0..<greedyArr.count {
-            if greedyArr[i].last! <= nums[0] && greedyArr[lastval].last! <= greedyArr[i].last! {
-                lastval = i
-                adding = true
-            }
-        }
-        if adding {
-            greedyArr[lastval] += nums
-        } else {
-            greedyArr.append(nums)
-        }
+        endtimes.append(interval.1)
     }
+    endtimes.sort()
 }
-print(greedyArr.count)
+print(endtimes.count)
+//func greedyAlgoritim(nums: [Int]) {
+//    if greedyArr.isEmpty {
+//        greedyArr.append(nums)
+//    } else {
+//        var lastval = 0
+//        var adding = false
+//        for i in 0..<greedyArr.count {
+//            if greedyArr[i].last! <= nums[0] && greedyArr[lastval].last! <= greedyArr[i].last! {
+//                lastval = i
+//                adding = true
+//            }
+//        }
+//        if adding {
+//            greedyArr[lastval] += nums
+//        } else {
+//            greedyArr.append(nums)
+//        }
+//    }
+//}
+//print(greedyArr.count)
