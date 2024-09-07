bf9b72ea293508be481206fb0ee7f70fd4bd704b
Sunday <sunhofficial@github.com>
2024-09-07 12:06:11 +0900

    2트: 보석쇼핑 Fail 1 시간초과

--- Alogrithm2/Alogrithm2/ETC/백준/보석쇼핑.swift
+++ Alogrithm2/Alogrithm2/ETC/백준/보석쇼핑.swift
@@ -1,28 +1,54 @@
 import Foundation
 
 func solution(_ gems:[String]) -> [Int] {
+//    var setgems = Set(gems)
+//    var temp = Int.max
+//    var answer = [0,0]
+//    var start = 1
+//    var end = setgems.count
+//    while start <= gems.count - setgems.count  {
+//        let setcount = Set(gems[start-1...end-1]).count
+//        if setcount == setgems.count {
+//            if temp > end - start {
+//                answer = [start,end]
+//                temp = end - start
+//            }
+//            start += 1
+//            end = start + setgems.count
+//            continue
+//        } else {
+//            end += 1
+//            if end > gems.count {
+//                start += 1
+//                end = start + setgems.count
+//            }
+//        }
+//    }
+//    return answer
     var setgems = Set(gems)
-    var temp = Int.max
-    var answer = [0,0]
-    var start = 1
-    var end = setgems.count
-    while start <= gems.count - setgems.count  {
-        let setcount = Set(gems[start-1...end-1]).count
-        if setcount == setgems.count {
-            if temp > end - start {
-                answer = [start,end]
-                temp = end - start
+    var gemDict = [String: Int]()
+    var start = 0
+    var end = 0
+    var answer = [1,gems.count]
+    let totalCount = setgems.count
+    var minLenth = Int.max
+    while end < gems.count {
+        gemDict[gems[end],default:  0] += 1
+    
+        while gemDict.count == totalCount {
+            if end - start < minLenth {
+                minLenth = end - start
+                answer = [start+1, end+1]
+                if minLenth + 1 == totalCount { break}
+            }
+            gemDict[gems[start]]! -= 1
+            if gemDict[gems[start]] == 0 {
+                gemDict.removeValue(forKey: gems[start])
             }
             start += 1
-            end = start + setgems.count
-            continue
-        } else {
-            end += 1
-            if end > gems.count {
-                start += 1
-                end = start + setgems.count
-            }
         }
+        end += 1
     }
     return answer
+    
 }
