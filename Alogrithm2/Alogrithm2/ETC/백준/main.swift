0238a75bc7808eb1d0a97b06534b4782144e4784
Sunday <sunhofficial@github.com>
2025-02-06 15:52:22 +0900

    2트: 2003 slidng window

--- Alogrithm2/Alogrithm2/ETC/백준/main.swift
+++ Alogrithm2/Alogrithm2/ETC/백준/main.swift
@@ -17,24 +17,44 @@ import Foundation
 
 //21921
 
+//let nk = readLine()!.split(separator: " ").map{Int(String($0))!}, n = nk[0], k = nk[1]
+//let maps = readLine()!.split(separator: " ").map{Int(String($0))!}
+//var sum = maps[0..<k].reduce(0, +)
+//var answer = sum
+//var temp = 1
+//
+//for i in k..<n {
+//    sum += maps[i] - maps[i-k]
+//    if sum == answer {
+//        temp += 1
+//    } else if sum > answer {
+//        temp = 1
+//        answer = sum
+//    }
+//}
+//if answer == 0 {
+//    print("SAD")
+//} else {
+//    print(answer)
+//    print(temp)
+//}
+
+
+// 2003 연속된 수의 합이 K
 let nk = readLine()!.split(separator: " ").map{Int(String($0))!}, n = nk[0], k = nk[1]
 let maps = readLine()!.split(separator: " ").map{Int(String($0))!}
-var sum = maps[0..<k].reduce(0, +)
-var answer = sum
-var temp = 1
-
-for i in k..<n {
-    sum += maps[i] - maps[i-k]
-    if sum == answer {
-        temp += 1
-    } else if sum > answer {
-        temp = 1
-        answer = sum
+var ans = 0
+var sum = 0
+var start = 0
+for end in 0..<n {
+    sum += maps[end]
+    while sum > k {
+        sum -= maps[start]
+        start += 1
     }
+    if sum == k {
+        ans += 1
+    }
+    
 }
-if answer == 0 {
-    print("SAD")
-} else {
-    print(answer)
-    print(temp)
-}
+print(ans)
