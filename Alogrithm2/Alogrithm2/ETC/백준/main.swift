d5eb692a10374e1cc5b4b08330f4ae8850858cbe
Sunday <sunhofficial@github.com>
2025-02-06 15:09:40 +0900

    2트: sliding window

--- Alogrithm2/Alogrithm2/ETC/백준/main.swift
+++ Alogrithm2/Alogrithm2/ETC/백준/main.swift
@@ -3,11 +3,14 @@ import Foundation
 //2559
 let nk = readLine()!.split(separator: " ").map{Int(String($0))!}, n = nk[0], k = nk[1]
 let maps = readLine()!.split(separator: " ").map{Int(String($0))!}
-var ans = 0
+var sum = maps[0..<k].reduce(0, +)
+var ans = sum
 
-for i in 0..<n-k {
-    ans = max(ans, maps[i..<i+k].reduce(0, +))
-    
+
+for i in k..<n {
+    sum += maps[i] - maps[i - k]
+    ans = max(sum,ans)
 }
 print(ans)
 // 1트 시간복잡도 Fail
+// tm
