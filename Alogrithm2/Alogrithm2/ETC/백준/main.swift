a6ea460f83bd976761ee5c5e1e2e17f870220166
Sunday <sunhofficial@github.com>
2025-02-06 15:25:14 +0900

    3트: 21921 sliding window

--- Alogrithm2/Alogrithm2/ETC/백준/main.swift
+++ Alogrithm2/Alogrithm2/ETC/백준/main.swift
@@ -1,16 +1,40 @@
 import Foundation
-
+// 슬라이딩 윈도우
 //2559
+//let nk = readLine()!.split(separator: " ").map{Int(String($0))!}, n = nk[0], k = nk[1]
+//let maps = readLine()!.split(separator: " ").map{Int(String($0))!}
+//var sum = maps[0..<k].reduce(0, +)
+//var ans = sum
+//
+//
+//for i in k..<n {
+//    sum += maps[i] - maps[i - k]
+//    ans = max(sum,ans)
+//}
+//print(ans)
+// 1트 시간복잡도 Fail
+// tm
+
+//21921
+
 let nk = readLine()!.split(separator: " ").map{Int(String($0))!}, n = nk[0], k = nk[1]
 let maps = readLine()!.split(separator: " ").map{Int(String($0))!}
 var sum = maps[0..<k].reduce(0, +)
-var ans = sum
-
+var answer = sum
+var temp = 1
 
 for i in k..<n {
-    sum += maps[i] - maps[i - k]
-    ans = max(sum,ans)
+    sum += maps[i] - maps[i-k]
+    if sum == answer {
+        temp += 1
+    } else if sum > answer {
+        temp = 1
+        answer = sum
+    }
 }
-print(ans)
-// 1트 시간복잡도 Fail
-// tm
+if answer == 0 {
+    print("SAD")
+} else {
+    print(answer)
+    print(temp)
+}
