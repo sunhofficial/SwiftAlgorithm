d4bd2aa1ebf81e69094c1f86a8ec7159165783d6
Sunday <sunhofficial@github.com>
2025-03-04 11:44:02 +0900

    sol: dp였음. 2중DP 근데 값이 있는지없는지를 bool값으로 관리

--- Alagorithm_swift/BFS,DFS/백준/main.swift
+++ Alagorithm_swift/BFS,DFS/백준/main.swift
@@ -8,21 +8,30 @@
 import Foundation
 let nsm = readLine()!.split(separator: " ").map{Int(String($0))!}, n = nsm[0], s = nsm[1], m = nsm[2]
 var inputs = readLine()!.split(separator: " ").map{Int(String($0))!}
-var answer = -1
-func dfs(_ current: Int, _ next: Int) {
-    if next == n {
-        if current > answer {
-            answer = current
+var dp = Array(repeating: Array(repeating: false, count: 1001), count: 51)
+if s + inputs[0] <= m {
+    dp[1][s + inputs[0]] = true
+}
+if s - inputs[0] >= 0 {
+    dp[1][s - inputs[0]] = true
+}
+for i in stride(from: 2, through: n, by: 1){
+    for j in 0...m {
+        if dp[i-1][j] {
+            if j - inputs[i - 1] >= 0 {
+                dp[i][j - inputs[i - 1] ] = true
+            }
+            if j + inputs[i-1] <= m {
+                dp[i][j + inputs[i - 1]] = true
+            }
         }
-        return 
-    }
-    if current + inputs[next] <= m {
-        dfs(current + inputs[next], next + 1)
-    }
-    if current - inputs[next] >= 0 {
-        dfs(current - inputs[next], next + 1)
     }
 }
-
-dfs(s, 0)
-print(answer)
+var index = -1
+for i in stride(from: m, through: 0, by: -1) {
+    if dp[n][i] {
+        index = i
+        break
+    }
+}
+print(index)
